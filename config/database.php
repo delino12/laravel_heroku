972db84850f4fd7927981d75966425a2d1d8be6a
Ekpoto Liberty Bernard <ekpoto.liberty@gmail.com>
2020-03-12 17:53:05 +0100

    update completed back to default

--- config/database.php
+++ config/database.php
@@ -38,9 +38,9 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', dirname(__DIR__).'/database/database.sqlite'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', false),
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
         'mysql' => [
